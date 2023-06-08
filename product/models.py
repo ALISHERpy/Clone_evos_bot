3288e0d1b8d9127d3c673147a85f4363fc23f06f
iMshaLch <76241077+iMshaLch@users.noreply.github.com>
2023-06-08 11:41:30 +0500

    create-mdl-in-prdct-aym

---
+++ product/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+
+    photo = models.CharField(max_length=128)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    number_of_product = models.PositiveSmallIntegerField(default=0)
+    is_mini = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
